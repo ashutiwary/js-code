22605bbdc5d4a91c5bcfd4ec1461ef5120ec34ac
ashutiwary <38155138+ashutiwary@users.noreply.github.com>
2024-06-14 05:39:32 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"Use Strict" //Treat all JS code as newer version
+
+// alert(3+3) //We are using node js, not browser
+
+/* console.log(3
+    +
+    3)*/ //Code Readabality should be high
+
+console.log("Test")
+
+let name = "Ashu"
+let Age = 12
+let isLoggedIn = true
+let state
+
+// number => 2 to power 53
+// bigInt
+// string => "
+// Boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //Undefined
+console.log(typeof null) //object
