89e6d2ed2f47bcaddbf65f5f997abcc0a4596d8d
ashutiwary <38155138+ashutiwary@users.noreply.github.com>
2024-06-16 10:35:44 +0000

    Comparisons of operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// ===
+
+console.log("2" === 2); // === check for data type in comparison while == check only for value
