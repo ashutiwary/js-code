d24b5431ebba53137261941b69a5a4c3c93267db
ashutiwary <38155138+ashutiwary@users.noreply.github.com>
2024-06-27 10:59:12 +0000

    function and if scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,7 +3,48 @@ let a = 200
 if(true){
     let a =10
     const b = 20
-    console.log(`Inner ${a}`);
+    // console.log(`Inner ${a}`);
 }
 
-console.log(a);
+// console.log(a);
+
+
+function one(){
+    const username = "Ashu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    
+    // console.log(website);  //it create error
+
+    two()
+
+}
+
+// one()
+
+
+if(true){
+    const username ="Ashu"
+    if(username === "Ashu"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++      Interesting        +++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
