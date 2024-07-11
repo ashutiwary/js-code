324d96da2311a2ef10fe5a7947e58628ef439a42
ashutiwary <38155138+ashutiwary@users.noreply.github.com>
2024-07-11 06:37:25 +0000

    array function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -21,4 +21,25 @@ const user = {
 
 // chai()
 
-const chai()
+// const chai = function(){
+//     let username = "Ashu"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "Ashu"
+//     console.log(this.username);
+// }
+// chai()
+
+// const addtwo = (num1,num2) => {
+//     return num1+num2;
+// }
+// const addtwo = (num1,num2) => num1+num2;
+
+// const addtwo = (num1,num2) => (num1+num2);
+
+const addtwo = (num1,num2) => ({username: "ashu"});
+
+console.log(addtwo(3,4));
