7b75b7d3988158a77a34924633d4c0b330c02959
ashutiwary <38155138+ashutiwary@users.noreply.github.com>
2024-07-11 06:28:15 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,24 @@
+const user = {
+    username: "Ashu",
+    price: 999,
+
+    welcomemessage: function(){
+        console.log(`${this.username}, Welcome to Website`);
+        // console.log(this);
+    }
+}
+
+// user.welcomemessage()
+// user.username = "Sam"
+// user.welcomemessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Ashu"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai()
